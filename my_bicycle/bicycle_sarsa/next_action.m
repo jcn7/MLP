40578471ea88a076c500d1b45234ddc919e6b877
unknown <Megan@Office-PC.(none)>
2011-12-09 19:17:36 -0500

    added comment

--- my_bicycle/bicycle_sarsa/next_action.m
+++ my_bicycle/bicycle_sarsa/next_action.m
@@ -1,3 +1,8 @@
+% next_action.m
+%
+% Choose the next action based on the best action given Q and a state
+%
+
 function [action] = next_action(Q, state, epsilon)
 
 	numactions = size(Q,2); % # of possible action
